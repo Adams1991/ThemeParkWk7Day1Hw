794c2fdc8b4bc97a8ecd122132f4f2f1ed98f365
Adams1991 <adams121@hotmail.co.uk>
2018-07-02 19:50:45 +0100

    is allowed to added to tobacco stand

--- src/main/java/StallType/TobaccoStall.java
+++ src/main/java/StallType/TobaccoStall.java
@@ -1,11 +1,19 @@
 package StallType;
 
+import People.Visitor;
+import ThemePark.ISecurity;
 import ThemePark.Stall;
 
-public class TobaccoStall extends Stall {
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, int parkingSpot){
         super(name, ownerName, parkingSpot);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() < 18){return false;}
+        return true;
     }
 
 
--- src/test/java/TobaccoStallTest.java
+++ src/test/java/TobaccoStallTest.java
@@ -1,3 +1,4 @@
+import People.Visitor;
 import StallType.TobaccoStall;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,10 +8,12 @@ import static org.junit.Assert.assertEquals;
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor;
 
     @Before
     public void setUp(){
         tobaccoStall = new TobaccoStall("Billy's Candy", "Billy", 300);
+        visitor = new Visitor("Jim", 200, 300, 16);
     }
 
     @Test
@@ -26,5 +29,10 @@ public class TobaccoStallTest {
     @Test
     public void canGetParkingSpot(){
         assertEquals(300, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void isAllowedToWorking(){
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor));
     }
 }
