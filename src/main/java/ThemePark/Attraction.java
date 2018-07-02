abedc970c691226d9b16c82534adedf0982d0438
Adams1991 <adams121@hotmail.co.uk>
2018-07-02 18:48:06 +0100

    dodgem get name working

--- src/main/java/ThemePark/Attraction.java
+++ src/main/java/ThemePark/Attraction.java
@@ -6,4 +6,8 @@ public abstract class Attraction {
         public Attraction(String name) {
             this.name = name;
         }
+
+    public String getName() {
+        return name;
+    }
 }
---
+++ src/test/java/DodgemsTest.java
@@ -0,0 +1,22 @@
+import AttractionType.Dodgems;
+import People.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DodgemsTest {
+
+    Dodgems dodgems;
+
+    @Before
+    public void setUp(){
+        dodgems = new Dodgems("Scary Dodge");
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Scary Dodge", dodgems.getName());
+    }
+
+}
