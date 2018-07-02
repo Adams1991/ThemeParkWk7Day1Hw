06c0057d53786a84779e25b7acf7ba3a932445ed
Adams1991 <adams121@hotmail.co.uk>
2018-07-02 20:37:08 +0100

    is allowed working on rollercoaster

--- src/main/java/AttractionType/Rollercoaster.java
+++ src/main/java/AttractionType/Rollercoaster.java
@@ -1,10 +1,18 @@
 package AttractionType;
 
+import People.Visitor;
 import ThemePark.Attraction;
+import ThemePark.ISecurity;
 
-public class Rollercoaster extends Attraction {
+public class Rollercoaster extends Attraction implements ISecurity {
 
     public Rollercoaster(String name){
         super(name);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() < 12 || visitor.getHeight() < 145) {return false;}
+        return true;
     }
 }
--- src/test/java/RollercoasterTest.java
+++ src/test/java/RollercoasterTest.java
@@ -1,4 +1,5 @@
 import AttractionType.Rollercoaster;
+import People.Visitor;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,14 +8,21 @@ import static org.junit.Assert.assertEquals;
 public class RollercoasterTest {
 
     Rollercoaster rollercoaster;
+    Visitor visitor;
 
     @Before
     public void setUp(){
         rollercoaster = new Rollercoaster("Scary Coaster");
+        visitor = new Visitor("Jim", 144, 300, 11);
     }
 
     @Test
     public void canGetName(){
         assertEquals("Scary Coaster", rollercoaster.getName());
+    }
+
+    @Test
+    public void isAllowedToWorking(){
+        assertEquals(false, rollercoaster.isAllowedTo(visitor));
     }
 }
